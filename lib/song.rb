bf09760cd88c118c68f886941ea6938fc26ab5d8
Chris Franklin <cf73021@gmail.com>
2018-08-03 18:38:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-
+  @@genre_count = {}
 
   def initialize(name, artist, genre)
     @name = name
@@ -30,7 +30,6 @@ class Song
   end
 
   def self.genre_count
-      genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1
