9c25e446b36fd40edb5f3882f4f0704e2130a57a
Chris Franklin <cf73021@gmail.com>
2018-08-03 17:09:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count +=1
-    @@artists << artist if @@artists.contains?(artist) == false
+    @@artists << artist if @@artists.contain?(artist) == false
     @@genres << genre
   end
 
